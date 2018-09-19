b454c377ce9126ee3099f5103e4c3bd665b826ba
kennyendowed <kenneyg50@gmail.com>
2018-09-19 17:07:06 +0100

    updated nav

--- resources/views/sitelayout/nav.blade.php
+++ resources/views/sitelayout/nav.blade.php
@@ -50,14 +50,6 @@
             </form>
           </li>
 
-    <li class="nav-item">
-      <a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Login</a>
-        </li>
-      <li class="nav-item">
-      <a class="nav-link js-scroll-trigger" href="{{ route('register') }}">Register</a>
-        </li>
-
-
             @endguest
 
 
