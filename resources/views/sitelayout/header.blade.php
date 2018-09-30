bf82f84fb72a2772fa55b2be54eebf59162fc080
kennyendowed <kenneyg50@gmail.com>
2018-09-30 16:52:03 +0100

    header

--- resources/views/sitelayout/header.blade.php
+++ resources/views/sitelayout/header.blade.php
@@ -16,14 +16,17 @@
     <!-- Bootstrap core CSS -->
     <link href="{{URL::asset('public/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
   <!-- Begin emoji-picker Stylesheets -->
-    <link href="{{URL::asset('public/lib/css/emoji.css')}}" rel="stylesheet">
+
     <!-- End emoji-picker Stylesheets -->
     <!-- Custom fonts for this template -->
     <link href="{{URL::asset('public/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+      @if (Route::currentRouteName() == ('comment')  || Route::currentRouteName() == ('create_topic') )
+          <link href="{{URL::asset('public/lib/css/emoji.css')}}" rel="stylesheet">
 <link href="http://www.cssscript.com/wp-includes/css/sticky.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+  @endif
     <!-- Custom styles for this template -->
     <link href="{{URL::asset('css/grayscale.css')}}" rel="stylesheet">
 <script src="{{ asset('js2/app.js') }}" defer></script>
